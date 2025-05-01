f686ea99e66ac48f6863f5a9dc1b8acadd9c8bd5
Benjamin Chislett <benjamin.chislett@centml.ai>
2025-05-01 14:12:34 +0000

    practical fine-tuning

--- query.py
+++ query.py
@@ -4,7 +4,8 @@ import tqdm
 import numpy as np
 import openai
 import asyncio
-from ragatouille import RAGTrainer
+from ragatouille import RAGPretrainedModel
+
 
 async def main():
 
@@ -84,28 +85,28 @@ async def main():
     # sample_cards = list(all_cards_by_name.keys())
     formatted_cards = [format_card(all_cards_by_name[card]) for card in sample_cards]
 
-    trainer = RAGTrainer(model_name="MTGColBERTv_0_0", pretrained_model_name="colbert-ir/colbertv2.0", language_code="en")
+    # RAG = RAGPretrainedModel.from_pretrained(".ragatouille/colbert/none/2025-04/30/18.51.27/checkpoints/colbert-100000")
 
-    with open("train_triples_easy_med_10x.jsonl", "r") as f:
-        train_triples = []
-        for line in f:
-            data = json.loads(line)
-            train_triples.append((data["query"], data["positive"], data["negative"]))
+    # documents = formatted_cards
+    # document_ids = sample_cards
+    # index_path = RAG.index(
+    #     index_name="mtg_cards_ft",
+    #     collection=documents,
+    #     document_ids=document_ids,
+    #     split_documents=False
+    # )
 
-    print(f"Loaded {len(train_triples)} training triples.")
+    RAG = RAGPretrainedModel.from_index(".ragatouille/colbert/indexes/mtg_cards_ft")
 
-    trainer.prepare_training_data(raw_data=train_triples, data_out_path="./train_data_v0_0/", all_documents=formatted_cards, mine_hard_negatives=False)
+    query = "cheap instant-speed cantrips"
+    res = RAG.search(query, k=100)
 
-    trainer.train(batch_size=32,
-                nbits=4, # How many bits will the trained model use when compressing indexes
-                maxsteps=100_000, # Maximum steps hard stop
-                use_ib_negatives=True, # Use in-batch negative to calculate loss
-                dim=128, # How many dimensions per embedding. 128 is the default and works well.
-                learning_rate=5e-6, # Learning rate, small values ([3e-6,3e-5] work best if the base model is BERT-like, 5e-6 is often the sweet spot)
-                doc_maxlen=256, # Maximum document length. Because of how ColBERT works, smaller chunks (128-256) work very well.
-                use_relu=False, # Disable ReLU -- doesn't improve performance
-                warmup_steps="auto", # Defaults to 10%
-    )
+    print(f"Found {len(res)} results")
+    for doc in res[:100]:
+        print(f"Document: {doc['document_id']}")
+        print(f"Score: {doc['score']}")
+        print(f"Document: {doc}")
+        print("-" * 80)
 
 if __name__ == '__main__':
     asyncio.run(main())
